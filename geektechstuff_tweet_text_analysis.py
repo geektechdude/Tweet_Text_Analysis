36a04663593f6dc3589faaa19758f3f3d04b7847
GeekTechDude <34283325+geektechdude@users.noreply.github.com>
2019-02-28 12:51:05 +0000

    Update geektechstuff_tweet_text_analysis.py
    Now auto retweets positive tweets and stores their IDs in a text file so that it doesn't auto retweet the same tweet multiple times.

--- geektechstuff_tweet_text_analysis.py
+++ geektechstuff_tweet_text_analysis.py
@@ -28,35 +28,52 @@ twitter = Twython(
 )
 
 def get_tweets(search_term):
+    # file to hold replied/retweeted Tweet ids to stop duplication
+    ids_replied_to = []
+    with open('ids_replied_to.txt', 'r') as filehandle:
+        filecontents = filehandle.readlines()
+        for line in filecontents:
+            # remove linebreak which is the last character of the string
+            current_place = line[:-1]
+            # add item to the list
+            ids_replied_to.append(current_place)
     # this searches Twitter
     results = twitter.cursor(twitter.search, q=search_term)
     # this then pulls each individual tweet
     for result in results:
-        # Tweet details that I may be interested in
         tweet_id = result['id_str']
-        tweet_text = result['text']
-        tweeted_time = result['created_at']
-        name = result['user']
-        tweet_screen_name = name['screen_name']
-        # Preparing the data to give to Azure
-        documents = {'documents' : [
-        {'id': tweet_id, 'language': 'en', 'text': tweet_text},
-        ]}
-        # Sending the data to Azure
-        response  = requests.post(sentiment_uri, headers=headers, json=documents)
-        # Getting response back from Azure
-        azure_response = response.json()
-        # Stripping the score out
-        azure_documents = azure_response['documents']
-        azure_score = azure_documents[0]['score']
-        if azure_score >= 0.6:
-            print(tweet_id,"\n", tweet_text, "from \n", tweet_screen_name)
-            print(azure_documents,"\n is postive")
-        elif azure_score == 0.5:
-            print(tweet_id,"\n", tweet_text, "from \n", tweet_screen_name)
-            print(azure_documents,"\n is neutral")
+        if tweet_id in ids_replied_to:
+            print("Skipping as already seen")
         else:
-            print(tweet_id,"\n", tweet_text, "from \n", tweet_screen_name)
-            print(azure_documents,"\n is negative")
+            # Tweet details that I may be interested in
+            tweet_text = result['text']
+            tweeted_time = result['created_at']
+            name = result['user']
+            tweet_screen_name = name['screen_name']
+            # Preparing the data to give to Azure
+            documents = {'documents' : [
+            {'id': tweet_id, 'language': 'en', 'text': tweet_text},
+            ]}
+            # Sending the data to Azure
+            response  = requests.post(sentiment_uri, headers=headers, json=documents)
+            # Getting response back from Azure
+            azure_response = response.json()
+            # Stripping the score out
+            azure_documents = azure_response['documents']
+            azure_score = azure_documents[0]['score']
+            if azure_score >= 0.6:
+                print(tweet_id,"\n", tweet_text, "from \n", tweet_screen_name)
+                print(azure_documents,"\n is postive")
+                print("Retweeting....")
+                twitter.retweet(id = tweet_id)
+                ids_replied_to.append(tweet_id)
+                with open('ids_replied_to.txt', 'w') as filehandle:
+                    filehandle.writelines("%s\n" % place for place in ids_replied_to)
+            elif azure_score == 0.5:
+                print(tweet_id,"\n", tweet_text, "from \n", tweet_screen_name)
+                print(azure_documents,"\n is neutral")
+            else:
+                print(tweet_id,"\n", tweet_text, "from \n", tweet_screen_name)
+                print(azure_documents,"\n is negative")
 
 get_tweets('geektechstuff')
